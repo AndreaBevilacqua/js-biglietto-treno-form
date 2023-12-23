f781045e47777f22377a19e939f1093e7003d718
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-23 18:16:08 +0100

    added script and fixed html

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
 console.log('JS OK')
 
 /* 
-1 - dichiarare un variabile km e etá
-2 - chiedere all'utente km e etá 
-3 - Calcolare il prezzo del biglietto
-4 - Creare lo sconto per l'etá
+1 - Recuper elementi dal dom 
+2 - agigungo un event listener al button di conferma
+3 - calcolo il prezzo del biglietto 
+4 - verifico se devo applicare lo sconto
 5 - Creare il prezzo finale
-6 - Stampare in pagina il prezzo
+6 - compongo il biglietto
 */
 
-const countKm = parseFloat(prompt("Quanto chilometri deve percorrere?"));
-const passAge = parseInt(prompt("Quanto anni ha?"));
+const nameField = document.getElementById('name');
+const kmsField = document.getElementById('kms');
+const ageField = document.getElementById('age');
 
-let prezzoBase = countKm * 0.21;
 
-if (passAge < 18) {
-    prezzoBase -= prezzoBase * 0.2;
-} else if (passAge >= 65) {
-    prezzoBase -= prezzoBase * 0.4;
-}
+const confirmButton = document.getElementById('create');
 
-let prezzoFinale = prezzoBase.toFixed(2);
+const ticketSection = document.getElementById('ticket-info');
+const passengerElement = document.getElementById('passenger-name');
+const typeElement = document.getElementById('ticket-type');
+const priceElement = document.getElementById('ticket-price');
 
-console.log("Il prezzo del biglietto è di: €" + prezzoFinale);
+
+const pricePerKms = 0.21;
+let rateName = 'Biglietto standard';
+
+confirmButton.addEventListener('click', function (){
+    const nameValue = nameField.value.trim();
+    const kmsValue = parseInt(kmsField.value);
+    const ageValue = ageField.value;
+
+    let price = pricePerKms * kmsValue;
+
+
+    if(ageValue === 'minor') {
+        rateName = 'Tariffa junior';
+        price *= 0.8;
+    } else if (ageValue === 'over'){
+        rateName = 'Tariffa Senior';
+        price *= 0.6;
+    }
+
+
+    passengerElement.innerText = nameValue;
+    typeElement.innerText = rateName;
+    priceElement.innerText = '$' + price.toFixed(2);
+
+})
